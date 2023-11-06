49d921961762a90602b91cc7161a32cdac80c735
Andre Aalborg <andreaalborg@hotmail.no>
2023-11-06 17:46:03 +0100

    added /picturesName route

--- routes/pictures.js
+++ routes/pictures.js
@@ -3,6 +3,20 @@ var router = express.Router();
 const fs = require('fs');
 var path = require('path');
 
+
+router.get('/:pictureName', function(req, res, next) {
+  var pictureName = req.params.pictureName;
+  var picturePath = path.join(__dirname, '..', 'pictures', pictureName);
+
+  fs.access(picturePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      // Filen finnes ikke
+      return res.status(404).send('Bildet ble ikke funnet');
+    }
+    // Filen finnes, send den tilbake som et svar
+    res.sendFile(picturePath);
+  });
+});
 // Or
 /* GET pictures listing. */
 router.get('/', function(req, res, next) {
