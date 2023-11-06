4983717a6b0dd70e7c7e17e61fec6edefb51a005
Andre Aalborg <andreaalborg@hotmail.no>
2023-11-06 19:35:40 +0100

    Cyclic changes

--- routes/pictures.js
+++ routes/pictures.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs');
 var path = require('path');
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
 
 
 router.get('/:pictureName', function(req, res, next) {
@@ -19,15 +21,44 @@ router.get('/:pictureName', function(req, res, next) {
 });
 // Or
 /* GET pictures listing. */
-router.get('/', function(req, res, next) {
-  const pictures = fs.readdirSync(path.join(__dirname, '../pictures/'));
-  res.render('pictures', { pictures: pictures});
+router.get('/', async function(req, res, next) {
+  try {
+    var params = {
+      Bucket: process.env.CYCLIC_BUCKET_NAME,
+      Delimiter: '/',
+      Prefix: 'public/'
+    };
+    var allObjects = await s3.listObjects(params).promise();
+    var keys = allObjects?.Contents.map(x => x.Key);
+
+    const pictures = await Promise.all(keys.map(async (key) => {
+      let my_file = await s3.getObject({
+        Bucket: process.env.CYCLIC_BUCKET_NAME,
+        Key: key,
+      }).promise();
+      return {
+        src: `data:image/jpeg;base64,${Buffer.from(my_file.Body).toString('base64')}`,
+        name: key.split("/").pop()
+      };
+    }));
+
+    // Send pictures to the view
+    res.render('pictures', { pictures: pictures });
+  } catch (error) {
+    // Handle errors here
+    console.error(error);
+    res.status(500).send('An error occurred');
+  }
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', async function(req, res, next) {
   const file = req.files.file;
-  fs.writeFileSync(path.join(__dirname, '../pictures/', file.name), file.data);
+  console.log(req.files);
+  await s3.putObject({
+    Body: file.data,
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Key: "public/" + file.name,
+  }).promise()
   res.end();
 });
-
 module.exports = router;
